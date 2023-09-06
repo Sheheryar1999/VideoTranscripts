5d2047261feac0087260d7e69f6249ad09dcd556
Sheheryar1999 <sheheryarali055@gmail.com>
2023-09-07 02:55:52 +0500

    removed stuff added stuff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-# Minami -「 Amewomatsu、」
-# vid = 'https://www.youtube.com/watch?v=766qmHTc2ro'
-
+import os
 import re
 from youtube_transcript_api import YouTubeTranscriptApi as yt
 from youtube_transcript_api.formatters import TextFormatter
@@ -14,22 +12,41 @@ def extract_video_id(youtube_link):
         return None
 
 def store_text(result):
+    ch = input("Make a new file or update an old one?\n Answer: ")
+    path = input("Enter File Name: ")
+    if ch == "old":
+        if os.path.exists(path):
+            try:
+                # Open the file in write mode ('w')
+                with open(path, "w", encoding="utf-8") as file:
+                    # You can now write data to the file
+                    file.write(result)
 
-    with open("storage.txt", "w", encoding= "utf-8") as file:
-        file.write(result)
-    print("Text stored in storage.txt")
+                # The file is automatically closed when the "with" block exits
+                print(f"File '{path}' was opened and written successfully.")
+            except Exception as e:
+                # Handle any exceptions that may occur during file operations
+                print(f"An error occurred: {e}")
+        else:
+            print("'{path}' does not exist. It cannot be opened.")
+
+    else:
+        path = path + ".txt"
+        with open(path, "w", encoding= "utf-8") as file:
+            file.write(result)
+    print(f"Text stored in {path}")
 
 
 def main():
 
     link = input("Enter Video Link: ")
     id = extract_video_id(link)
-    print(f"Invalid Input {e}")
     # print(id)
     result = yt.get_transcript(id, languages= ['en'])
     formatter = TextFormatter()
     result = formatter.format_transcript(result)
 
+    store_text(result)
 
 if __name__ == "__main__":
     main()
